da839c6190e95e1e3a203e404caf0ef09f38bbc5
VNATdev-ops <vuanhtuan18092004@gmail.com>
2024-06-05 16:11:50 +0700

    test commit

--- QuanLy_KhachSan/Form1.Designer.cs
+++ QuanLy_KhachSan/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(179, 157);
+            this.button1.Location = new System.Drawing.Point(37, 45);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 82);
             this.button1.TabIndex = 0;
